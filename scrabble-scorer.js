9f2f476d4152395d4cc5f5983908fd15dd242595
YumanGong <gongymsilvia@gmail.com>
2023-10-05 18:34:26 -0500

    new commit

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -3,56 +3,139 @@
 const input = require("readline-sync");
 
 const oldPointStructure = {
-  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-  2: ['D', 'G'],
-  3: ['B', 'C', 'M', 'P'],
-  4: ['F', 'H', 'V', 'W', 'Y'],
-  5: ['K'],
-  8: ['J', 'X'],
-  10: ['Q', 'Z']
+   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+   2: ['D', 'G'],
+   3: ['B', 'C', 'M', 'P'],
+   4: ['F', 'H', 'V', 'W', 'Y'],
+   5: ['K'],
+   8: ['J', 'X'],
+   10: ['Q', 'Z']
 };
 
 function oldScrabbleScorer(word) {
-	word = word.toUpperCase();
-	let letterPoints = "";
- 
-	for (let i = 0; i < word.length; i++) {
- 
-	  for (const pointValue in oldPointStructure) {
- 
-		 if (oldPointStructure[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
-		 }
- 
-	  }
-	}
-	return letterPoints;
- }
+   word = word.toUpperCase();
+   let letterPoints = "";
+   for (let i = 0; i < word.length; i++) {
+      for (const pointValue in oldPointStructure) {
+         if (oldPointStructure[pointValue].includes(word[i])) {
+            letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+         }
+      }
+   }
+   return letterPoints;
+}
+
+function transform(oldPointStructure) {
+   let newPointStructure = {};
+   for (let keys in oldPointStructure) {
+      let letters = oldPointStructure[keys];
+      for (let i = 0; i < letters.length; i++) {
+         let letter = letters[i];
+         let lowercaseLetter = letter.toLowerCase();
+         // Assign the point value to the lowercase letter in the new structure
+         newPointStructure[lowercaseLetter] = Number(keys);
+      }
+   }
+   return newPointStructure;
+};
+
+let newPointStructure = transform(oldPointStructure);
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   let word = input.question("Let's play some scrabble! \n\nEnter a word:");
+   // console.log(oldScrabbleScorer(word));
+   return word;
 };
 
-let simpleScorer;
+//Define a function that takes a word as a parameter and returns a numerical score. Each letter within the word is worth 1 point.
+let simpleScorer = function (word) {
+   word = word.toUpperCase().split(" ").join("");
+   let numericalScore = word.length;
+   return numericalScore;
+}
+//Define a function that takes a word as a parameter and returns a score. Each vowel within the word is worth 3 points, and each consonant is worth 1 point.
+let vowelBonusScorer = function (word) {
+   word = word.toUpperCase();
+   let numericalScore = 0;
+   for (let i = 0; i < word.length; i++) {
+      const letter = word[i];
+      if ('AEIOU'.includes(letter)) {
+         numericalScore += 3;
+      } else if ('BCDFGHJKLMNPQRSTVWXYZ'.includes(letter)) {
+         numericalScore += 1;
+      }
+   }
+   return numericalScore;
+};
 
-let vowelBonusScorer;
+let scrabbleScorer = function (word) {
+   word = word.toLowerCase();
+   let numericalScore = 0;
+   for (let i = 0; i < word.length; i++) {
+      for (const item in newPointStructure) {
+         if (item === word[i]) {
+            numericalScore += Number(newPointStructure[item]);
+         }
+      }
+   }
+   return numericalScore;
+};
 
-let scrabbleScorer;
+// console.log(scrabbleScorer("foo"));
 
-const scoringAlgorithms = [];
+//scoring object
 
-function scorerPrompt() {}
+let simpleScore = {
+   name: "Simple Score",
+   description: "Each letter is worth 1 point.",
+   scorerFunction: simpleScorer
+};
 
-function transform() {};
 
-let newPointStructure;
+let BonusVowels = {
+   name: "Bonus Vowels",
+   description: "Vowels are 3 pts, consonants are 1 pt.",
+   scorerFunction: vowelBonusScorer
+};
+
+let scrabble = {
+   name: "Scrabble",
+   description: "The traditional scoring algorithm.",
+   scorerFunction: scrabbleScorer
+};
+
+let scoringAlgorithms = [simpleScore, BonusVowels, scrabble];
+
+function scorerPrompt() {
+   let word = initialPrompt();
+   let selectedIDs = input.question("Which scoring algorithm would you like to use?\n\n0 - Simple: One point per character\n1 - Vowel Bonus: Vowels are worth 3 points\n2 - Scrabble: Uses scrabble point system\nEnter 0, 1, or 2: ");
+   if (Number(selectedIDs) === 0) {
+      // Simple scoring1
+      console.log(`score for '${word}':${scoringAlgorithms[0].scorerFunction(word)}`);
+   } else if (Number(selectedIDs) === 1) {
+      // owel bonus scoring 
+      console.log(`score for '${word}':${scoringAlgorithms[1].scorerFunction(word)}`);
+   } else if (Number(selectedIDs) === 2) {
+      //  Scrabble scoring
+      console.log(`score for '${word}':${scoringAlgorithms[2].scorerFunction(word)}`);
+   } else {
+      console.log("Invalid input. Please enter 0, 1, or 2.");
+      selectedIDs = input.question("Which scoring algorithm would you like to use?\n\n0 - Simple: One point per character\n1 - Vowel Bonus: Vowels are worth 3 points\n2 - Scrabble: Uses scrabble point system\nEnter 0, 1, or 2: ");
+   }
+}
+
+// console.log(newPointStructure);
+// console.log("Scrabble scoring values for");
+// console.log("letter a: ", newPointStructure.a);
+// console.log("letter j: ", newPointStructure.j);
+// console.log("letter z: ", newPointStructure["z"]);
 
 function runProgram() {
-   initialPrompt();
-   
+   // initialPrompt();
+   scorerPrompt();
 }
 
 // Don't write any code below this line //
@@ -66,6 +149,6 @@ module.exports = {
    scrabbleScorer: scrabbleScorer,
    scoringAlgorithms: scoringAlgorithms,
    newPointStructure: newPointStructure,
-	runProgram: runProgram,
-	scorerPrompt: scorerPrompt
+   runProgram: runProgram,
+   scorerPrompt: scorerPrompt
 };
--- test/scrabble-scorer.test.js
+++ test/scrabble-scorer.test.js
@@ -1,44 +1,44 @@
 const solution = require('../scrabble-scorer');
 
-describe("Scrabble Scorer solution", function() {
+describe("Scrabble Scorer solution", function () {
 
 	// transform tests //
-	it("transform returns an object", function() {
+	it("transform returns an object", function () {
 		let transformedObj = solution.transform(solution.oldPointStructure);
 		expect(transformedObj).toBeInstanceOf(Object);
 	});
 
-	it("transform returns an object that is not empty", function() {
+	it("transform returns an object that is not empty", function () {
 		let transformedObj = solution.transform(solution.oldPointStructure);
 		expect(Object.keys(transformedObj)).not.toBeNull();
 	});
 
-	it("transform returns an object with letter keys", function() {
+	it("transform returns an object with letter keys", function () {
 		let transformedObj = solution.transform(solution.oldPointStructure);
 		let letterKeys = Object.keys(transformedObj);
-		
+
 		let lettersEx = /[a-z]/g;
 
 		// .every() returns true if each item in the array passes the match
-		let expected = letterKeys.every(function(l) {
+		let expected = letterKeys.every(function (l) {
 			return l.match(lettersEx);
 		});
-		
+
 		expect(expected).toBe(true);
 	});
 
-	it("transform returns an object with integer values", function() {
+	it("transform returns an object with integer values", function () {
 		let transformedObj = solution.transform(solution.oldPointStructure);
 		let numberVals = Object.values(transformedObj);
 
-		let expected = numberVals.every(function(n) {
+		let expected = numberVals.every(function (n) {
 			return typeof n == 'number';
 		});
 		expect(expected).toBe(true);
 	});
 
 	// // newPointStructure tests //
-	it("newPointStructure contains the correct key-value pairs", function() {
+	it("newPointStructure contains the correct key-value pairs", function () {
 		expect(solution.newPointStructure).toEqual(expect.objectContaining({
 			a: 1,
 			e: 1,
@@ -70,29 +70,29 @@ describe("Scrabble Scorer solution", function() {
 	});
 
 	// simpleScorer tests //
-	it("contains a simpleScorer function", function() {
+	it("contains a simpleScorer function", function () {
 		expect(solution.simpleScorer).toEqual(expect.any(Function));
 	});
 
-	it("simpleScorer returns an integer score", function() {
+	it("simpleScorer returns an integer score", function () {
 		expect(typeof solution.simpleScorer('foo')).toBe('number');
 	});
 
-	it("simpleScorer returns a score equal to the length of its input", function() {
+	it("simpleScorer returns a score equal to the length of its input", function () {
 		expect(solution.simpleScorer('foo')).toBe(3);
 		expect(solution.simpleScorer('')).toBe(0);
 	});
 
 	// vowelBonusScorer tests //
-	it("contains a vowelBonusScorer function", function() {
+	it("contains a vowelBonusScorer function", function () {
 		expect(solution.vowelBonusScorer).toEqual(expect.any(Function));
 	});
 
-	it("vowelBonusScorer returns an integer score", function() {
+	it("vowelBonusScorer returns an integer score", function () {
 		expect(typeof solution.vowelBonusScorer('foo')).toBe('number');
 	});
 
-	it("vowelBonusScorer returns three points per vowel", function() {
+	it("vowelBonusScorer returns three points per vowel", function () {
 		expect(solution.vowelBonusScorer('a')).toBe(3);
 		expect(solution.vowelBonusScorer('e')).toBe(3);
 		expect(solution.vowelBonusScorer('i')).toBe(3);
@@ -103,23 +103,23 @@ describe("Scrabble Scorer solution", function() {
 		expect(solution.vowelBonusScorer('aei')).toBe(9);
 	});
 
-	it("vowelBonusScorer returns one point per consonant", function() {
+	it("vowelBonusScorer returns one point per consonant", function () {
 		expect(solution.vowelBonusScorer('foo')).toBe(7);
 		expect(solution.vowelBonusScorer('bar')).toBe(5);
-		
+
 	});
 
 	// scrabbleScorer tests //
-	it("contains a scrabbleScorer function", function() {
+	it("contains a scrabbleScorer function", function () {
 		expect(solution.scrabbleScorer).toEqual(expect.any(Function));
 	});
 
-	it("scrabbleScorer returns an integer score", function() {
+	it("scrabbleScorer returns an integer score", function () {
 		let newPointStructure = solution.transform(solution.oldPointStructure);
 		expect(typeof solution.scrabbleScorer('foo', newPointStructure)).toBe('number');
 	});
 
-	it("scrabbleScorer uses transform() to score a word", function() {
+	it("scrabbleScorer uses transform() to score a word", function () {
 		let newPointStructure = solution.transform(solution.oldPointStructure);
 		expect(solution.scrabbleScorer('foo', newPointStructure)).toBe(6);
 		expect(solution.scrabbleScorer('bar', newPointStructure)).toBe(5);
@@ -127,11 +127,11 @@ describe("Scrabble Scorer solution", function() {
 	});
 
 	// scoringAlgorithms tests //
-	it("contains a scoringAlgorithms array of three scoring objects", function() {
+	it("contains a scoringAlgorithms array of three scoring objects", function () {
 		expect(solution.scoringAlgorithms.length).toBe(3);
 	});
 
-	it("scoringAlgorithms contain three scoring objects", function() {
+	it("scoringAlgorithms contain three scoring objects", function () {
 		expect(solution.scoringAlgorithms[0]).toEqual(expect.objectContaining({
 			scorerFunction: solution.simpleScorer
 		}));
@@ -142,5 +142,4 @@ describe("Scrabble Scorer solution", function() {
 			scorerFunction: solution.scrabbleScorer
 		}));
 	});
-	
 });
